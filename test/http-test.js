cdc3cfece0300ab223a46c182170ee02e484df6f
kilpatty <seanpkilgarriff@gmail.com>
2019-03-17 15:02:23 -0500

    add transaction functions and tests

--- test/http-test.js
+++ test/http-test.js
@@ -53,4 +53,73 @@ describe("Http Tests", function() {
     assert(result.branch);
     assert(result.root);
   });
+
+  it("should return fee estimate", async () => {
+    let result = await client.estimateFee(10);
+
+    assert(typeof result.fee === "number");
+  });
+
+  it("should return the specific nomenclate instance's fee for relaying txs", async () => {
+    let result = await client.relayFee();
+
+    assert(typeof result.fee === "number");
+  });
+
+  it("should return raw transaction hex", async () => {
+    let result = await client.getTransaction(
+      "5c500def0d0ebf0fe325d66ce7e56992809d5da7837c6dd563cbea272e8ab201"
+    );
+
+    assert(result.hex);
+  });
+
+  it("should return raw transaction hex and merkle proof", async () => {
+    let result = await client.getTransaction(
+      "5c500def0d0ebf0fe325d66ce7e56992809d5da7837c6dd563cbea272e8ab201",
+      true
+    );
+
+    assert(result.hex);
+    assert(result.pos);
+    assert(result.block_height);
+    assert(result.merkle);
+  });
+
+  it("should return verbose transaction information", async () => {
+    let result = await client.getTransaction(
+      "5c500def0d0ebf0fe325d66ce7e56992809d5da7837c6dd563cbea272e8ab201",
+      false,
+      true
+    );
+
+    assert(result.txid);
+    assert(result.hash);
+  });
+
+  it("should return verbose transaction information and merkle proof", async () => {
+    let result = await client.getTransaction(
+      "5c500def0d0ebf0fe325d66ce7e56992809d5da7837c6dd563cbea272e8ab201",
+      true,
+      true
+    );
+
+    assert(result.txid);
+    assert(result.hash);
+    assert(result.merkle);
+    assert(result.hex);
+  });
+
+  //TODO test broadcast, but likely can only test on regtest networks.
+  //
+  it("should return merkle proof information for tx", async () => {
+    let result = await client.getMerkle(
+      "0ded93a1f39b9b0b3692dbfdb89dce9a8777608f27de365ecb8cf04d94450e88",
+      9634
+    );
+
+    assert(result.merkle);
+    assert(result.pos);
+    assert(result.block_height);
+  });
 });
